ae49fe1b816be656392f400aa8ef8deb482c6608
Jmaun22 <98067577+Jmaun22@users.noreply.github.com>
2022-05-21 18:58:52 -0400

    switches between breaks and working

--- src/motivated_assets/src/componets/timer/testTimer.jsx
+++ src/motivated_assets/src/componets/timer/testTimer.jsx
@@ -1,18 +1,42 @@
 import React, { useEffect, useState } from "react";
+import { ExploreOffOutlined } from "../../../../../node_modules/@material-ui/icons/index";
 
 const TestTimer = (props) => {
 
-    console.log(props.seconds);
-    console.log('TestTimer');
 
-    const totalTimeinSeconds = Number(props.seconds) + (Number(props.minutes)) * 60 + (Number(props.hours) * 60 * 60)
-console.log(totalTimeinSeconds);
+
+    // working min
+    // Number(props.seconds);
+
+    var counter = 0;
+    const workingMin =  Number(props.seconds);
+
+    const shortBreakMin = (Number(props.minutes));;
+    // (Number(props.minutes));
+
+    const longBreakMin = (Number(props.hours));
+    // (Number(props.hours));
+
+
+    // short break
+
+    // long break
+    // var workSec = workingMin * 60 
+    const workSec = workingMin;
+    const shortBreakSec = shortBreakMin;
+    const longBreakSec= longBreakMin;
+
+
+//     const totalTimeinSeconds = Number(props.seconds) + (Number(props.minutes)) * 60 + (Number(props.hours) * 60 * 60)
+// console.log(totalTimeinSeconds);
 
   
-  const [count, setCount] = useState(Number(totalTimeinSeconds)); // seconds
+const [count, setCount] = useState(Number(workSec)); // seconds
   const [hour, setHour] = useState(0);
   const [minute, setMinute] = useState(0);
   const [second, setSecond] = useState(0);
+  const [sessionType, setSession] = useState('Click start to get started')
+  const [reps, setReps] = useState(0)
 
   function secondsToTime(secs) {
     var hours = Math.floor(secs / (60 * 60));
@@ -26,6 +50,8 @@ console.log(totalTimeinSeconds);
       s: seconds
     };
   }
+
+  
   useEffect(() => {
     if (count >= 0 && isActive) {
       const secondsLeft = setInterval(() => {
@@ -34,12 +60,41 @@ console.log(totalTimeinSeconds);
         setHour(timeLeftVar.h);
         setMinute(timeLeftVar.m);
         setSecond(timeLeftVar.s);
+      
       }, 1000);
       return () => clearInterval(secondsLeft);
-    } else {
-      console.log("timeout");
+    } else if(!isActive){
+        console.log('when will you work?');
+        console.log('not active');
     }
-  }, [count]);
+    
+    else if(isActive){
+
+        if(counter === 2){
+            console.log('yikes');
+        } else{
+            console.log('maybe its working');
+            ++counter
+            setReps(reps + 1)
+            swithTimer(reps)
+            console.log('first reps' + reps);
+        }
+     
+   
+        console.log('active');
+        console.log(reps + 'why you not work');
+        console.log(isActive);
+        console.log(count);
+        console.log(counter + 'counter');
+     
+
+   
+       
+      
+        }
+  }, [count, reps]);
+
+
 
 
   const [seconds, setSeconds] = useState(count);
@@ -54,17 +109,51 @@ function reset() {
     setIsActive(false);
 }
 
+var done = false;
+
+function swithTimer(reps) {
+
+    if(reps % 8 == 0){
+        setCount(longBreakSec);
+        setSession('long Break')
+        console.log('2');
+     
+    }else if(reps % 2 == 0){
+        setCount(shortBreakSec);
+        setSession('short break');
+
+        console.log('counting');
+
+    } else {
+        setCount(workSec);
+        setSession('working');
+    }
+
+}
+
 useEffect(() => {
     let interval = null;
     if (isActive) {
         interval = setInterval(() => {
 
             setCount(count => count - 1);
-            console.log(seconds)
+            // 
+            console.log(seconds + 'is this it?')
             if (seconds === 1) {
+
+                if(!done){
+                    setReps(reps + 1)
+                    swithTimer(reps)
+                    console.log('reps 2 ' + reps);
+                    counter++
+                    swithTimer(counter)
+                }
+                else{
+                    ++counter
                 clearInterval(interval);
                 setIsActive()
                 setIsActive(!isActive);
+                }
 
             }
         }, 1000);
@@ -72,17 +161,23 @@ useEffect(() => {
     }
 
 
-    else if (!isActive || count === 0) {
+    else if (!isActive || count === 0 && done) {
         console.log('cleared');
-        console.log(seconds)
+        console.log(seconds + 'seconds')
         clearInterval(interval);
     }
+
     return () => clearInterval(interval);
 }, [isActive, count]);
 
 
+
+
+
+
   return (
     <>
+    <h2> { sessionType}: {reps}</h2>
       <h1>
         {hour < 9 ? "0" + hour : hour} : {minute < 9 ? "0" + minute : minute} :
         {second < 9 ? "0" + second : second}
@@ -97,7 +192,7 @@ useEffect(() => {
             </div>
     </>
   );
-}
+  }
 
 export default TestTimer;
 
